511213e72a9c7f6869427f9e3de370cc92ccaeaf
1Samuel17 <AtDz@LocalHost.Com>
2026-02-09 22:58:56 -0600

    Modified template to preferred starting structure

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// A template for Rust CLI applications
 #[derive(Parser, Debug)]
 #[command(name = "rust-cli-template")]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Name to greet
-    #[arg(short, long, default_value = "World")]
-    name: String,
+    
+    /// Subcommand to run
+    #[command(subcommand)]
+    command: Option<Commands>,
 
     /// Enable verbose output
     #[arg(short, long)]
     verbose: bool,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Greet {
+        /// Name to greet
+        #[arg(short, long, default_value = "World")]
+        name: String,
+    },
 }
 
 fn main() -> Result<()> {
@@ -22,7 +32,14 @@ fn main() -> Result<()> {
         println!("Running in verbose mode...");
     }
 
-    println!("Hello, {}!", cli.name);
+    match &cli.command {
+        Some(Commands::Greet { name }) => {
+            println!("Hello, {}!", name);
+        }
+        None => {
+            println!("No subcommand was used. Use --help for more information.");
+        }
+    }
 
     Ok(())
 }
